7ee63e6a50bff24f0f146467645638239c1bf773
Salomon Orrego Martinez <sorregom@psl.com.co>
2019-04-10 16:47:26 -0500

    adding steps as pending

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,2 +1,18 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
+
+  Given('today is Sunday', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('I ask whether it\'s Friday yet', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('I should be told {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  
